73dba34ec727e45b19376468afe9ceadcf178052
Jonathan Overy <jovery90@gmail.com>
2023-06-25 18:39:04 -0500

    Update Python.py

--- Coding Basics/Python.py
+++ Coding Basics/Python.py
@@ -1,18 +1,27 @@
 #This doc contains the basics for Python
 
+#tabs - reminder, Python is strict for tabs
+
 #comments
 # Single-line comment
 
-##
-Multi-line comment
-##
+"""
+Multi-line string
+"""
 
 #vars
+someVar = 123
 
 #functions
+def my_function():
+  #tabbed over, then logic
 
 #string interpolation
+print(“Name:”, name)
 
 #conditionals
+if, elif, else
+and, or, not
 
 #operators
+==, !=, >=, <=
