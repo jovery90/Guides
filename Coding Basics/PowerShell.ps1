ff9b6a2b89c770f2f230a63853a2148028cfaeb8
Jonathan Overy <jovery90@gmail.com>
2023-06-25 15:39:51 -0500

    Create PowerShell.ps1

---
+++ Coding Basics/PowerShell.ps1
@@ -0,0 +1,17 @@
+# This doc contains the basics for PowerShell
+
+#vars
+$someVar
+
+#functions
+#params are separated by spaces, not commas
+function SomeFunction(){}
+
+#string interpolation
+"Some string and a var - $(someVar)"
+
+#conditionals
+if, elseif, else
+
+#operators
+-eq, , -ne, -lt, -gt
