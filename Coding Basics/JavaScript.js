e369a726e918d153f528d23c815d0ee6ea6d46b4
Jonathan Overy <jovery90@gmail.com>
2023-06-25 15:55:16 -0500

    Update JavaScript.js

--- Coding Basics/JavaScript.js
+++ Coding Basics/JavaScript.js
@@ -1,1 +1,26 @@
-//JS
+// This doc contains the basics for PowerShell
+
+//comments
+// Single-line comment
+
+/*
+Multi-line comment
+*/
+
+//vars
+// const, let, and var
+var someVariable = 123
+
+//functions
+function SomeFunction(){}
+
+//string interpolation
+var StringInterpolation = This is a string…” + someVar
+
+//conditionals
+if, else if, else
+
+//operators (and comparison operators), uses PEMDAS
+= != == (value) === (value and type) !== ++ -- * / += -= % < > <= >=
+
+// https://www.w3schools.com/js/js_operators.asp
