e7957d8c55b1b03d10c976ad880287b4ed327e0b
Jonathan Overy <jovery90@gmail.com>
2023-06-30 13:16:44 -0500

    Update Cleanup.ps1 - Details added

--- PowerShell Capabilities/Cleanup.ps1
+++ PowerShell Capabilities/Cleanup.ps1
@@ -1,4 +1,4 @@
-# Cleanup.ps1 will remove all files from specified directory
+# Cleanup.ps1 will remove all files from specified directory (this version doesn't check folders, see Reference for other options)
 # $FolderPath = Full file path.  $Days = Number of days used as buffer of file history to delete
 # Reference:  https://www.thomasmaurer.ch/2010/12/powershell-delete-files-older-than/
 
