d34ecdab9209d8062fa45f9c4c6e94acef02f02d
Jonathan Overy <jovery90@gmail.com>
2023-07-07 17:05:07 -0500

    Create CopyTables.ps1

---
+++ PowerShell Capabilities/CopyTables.ps1
@@ -0,0 +1,3 @@
+# Copy SQL Tables with PowerShell Copy-DbaDbTableData
+
+Copy-DbaDbTableData -SqlInstance "Source Sql Server" -Destination "Target Sql Server" -Database "Source DB" -DestinationDatabase "Target DB" -Table "Source Table" -DestinationTable "Target Table" -AutoCreateTable
